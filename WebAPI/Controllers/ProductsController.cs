5215ed3bdb9c028006145f3737bee5148a213667
ebrar kadir çetin <cetinebrarkadir@gmail.com>
2022-11-09 01:46:21 +0300

    11

---
+++ WebAPI/Controllers/ProductsController.cs
@@ -0,0 +1,35 @@
+﻿using Business.Abstract;
+using Business.Concrete;
+using DataAccess.Concrete.EntityFramework;
+using Entities.Concrete;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+
+    
+    public class ProductsController : ControllerBase
+    {
+        //loosely coupled - gevşek bağlılık
+        //Naming convention -- isimlendirme strandartı
+        //Ioc container -inversion on control
+        IProductService _productService;//field
+
+        public ProductsController(IProductService productService)
+        {
+            _productService = productService;
+        }
+
+        [HttpGet]
+        public List<Product> Get()
+        {
+            //dependency chain -- bağımlılık zinciri
+            IProductService productService = new ProductManager(new EfProductDal());//ıproductservice productmanager referansını tutabiliyor
+            var result = _productService.GetAll();
+            return result.Data;
+
+        }
+}
