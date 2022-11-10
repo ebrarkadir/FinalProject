6fcfc471edce62a598398a69df45c78370801d4d
ebrar kadir çetin <cetinebrarkadir@gmail.com>
2022-11-11 02:10:30 +0300

    12

--- Business/Concrete/ProductManager.cs
+++ Business/Concrete/ProductManager.cs
@@ -1,13 +1,19 @@
 ﻿using Business.Abstract;
 using Business.Constants;
+using Business.ValidationRules.FluentValidation;
+using Core.CrossCuttingConcerns.Validation;
+using Core.Entities;
 using Core.Utilities.Results;
 using DataAccess.Abstract;
 using DataAccess.Concrete.InMemory;
 using Entities.Concrete;
 using Entities.DTOs;
+using FluentValidation;
 using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
+using System.Runtime.ConstrainedExecution;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -24,11 +30,10 @@ namespace Business.Concrete
 
         public IResult Add(Product product)
         {
-            if (product.ProductName.Length < 2)
-            {
-                //magic strings
-                return new ErrorResult(Messages.ProductNameInvalid);
-            }
+            //validationS
+            ValidationTool.Validate(new ProductValidator(), product);
+            //business
+
             _productDal.Add(product);
 
             return new SuccessResult(Messages.ProductAdded);
@@ -38,7 +43,7 @@ namespace Business.Concrete
         {
             // iş kodları
             //Yetkisi var mı ?
-            if (DateTime.Now.Hour ==22)
+            if (DateTime.Now.Hour == 22)
             {
                 return new ErrorDataResult<List<Product>>(Messages.MaintenanceTime);
             }
@@ -46,17 +51,17 @@ namespace Business.Concrete
             {
                 return new SuccessDataResult<List<Product>>(_productDal.GetAll(), Messages.ProductListed);
             }
-            
+
         }
 
         public IDataResult<List<Product>> GetAllByCategoryId(int id)
         {
-            return new SuccessDataResult<List<Product>>(_productDal.GetAll(p=>p.CategoryId == id));
+            return new SuccessDataResult<List<Product>>(_productDal.GetAll(p => p.CategoryId == id));
         }
 
         public IDataResult<List<Product>> GetAllByUnitPrice(decimal min, decimal max)
         {
-            return new SuccessDataResult<List<Product>>(_productDal.GetAll(p=>p.UnitPrice>=min && p.UnitPrice<=max));
+            return new SuccessDataResult<List<Product>>(_productDal.GetAll(p => p.UnitPrice >= min && p.UnitPrice <= max));
         }
 
         public IDataResult<Product> GetById(int productId)
@@ -64,7 +69,7 @@ namespace Business.Concrete
             return new SuccessDataResult<Product>(_productDal.Get(p => p.ProductId == productId));
         }
 
-        
+
 
         public IDataResult<List<ProductDetailDto>> GetProductDetails()
         {
@@ -76,7 +81,7 @@ namespace Business.Concrete
             {
                 return new SuccessDataResult<List<ProductDetailDto>>(_productDal.GetProductDetails());
             }
-            
+
         }
     }
 }
